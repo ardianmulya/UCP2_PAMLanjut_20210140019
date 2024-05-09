3577854ed7c1165ca266ea9302db64aaae0148fb
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 18:55:10 +0700

    membuat function fetchKuliner

--- lib/service/kuliner_service.dart
+++ lib/service/kuliner_service.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -25,5 +26,22 @@ class KulinerService {
     }
 
     return await http.Response.fromStream(await request.send());
+  }
+
+    Future<List<dynamic>> fetchKuliner() async {
+    var response = await http.get(
+        getUri(
+          endpoint,
+        ),
+        headers: {
+          "Accept": "application/json",
+        });
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedResponse = json.decode(response.body);
+      return decodedResponse['kuliner'];
+    } else {
+      throw Exception('Tidak dapat mengambil data: ${response.reasonPhrase}');
+    }
   }
 }
