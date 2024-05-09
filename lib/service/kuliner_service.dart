0bc2539f0958150aafb19d675ae5e271b49cfa26
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 18:53:15 +0700

    membuat function addKuliner

--- lib/service/kuliner_service.dart
+++ lib/service/kuliner_service.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 class KulinerService {
 
   final String baseUrl = 'http://192.168.224.122/api/';
@@ -5,5 +9,21 @@ class KulinerService {
 
     Uri getUri(String path) {
     return Uri.parse("$baseUrl$path");
+  }
+  Future<http.Response> addKuliner(Map<String, String> data, File? file) async {
+    var request = http.MultipartRequest(
+      'POST',
+      getUri(endpoint),
+    )
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
+
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath('foto', file.path));
+    } else {
+      request.files.add(http.MultipartFile.fromString('foto', ''));
+    }
+
+    return await http.Response.fromStream(await request.send());
   }
 }
