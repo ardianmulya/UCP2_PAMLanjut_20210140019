97f12d29ce741345da8dd9da7d5f5506b7f1d3ec
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 19:50:29 +0700

    membuat variabel dari class KulinerController

--- lib/screen/form_kuliner_screen.dart
+++ lib/screen/form_kuliner_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp2_pam/controller/kuliner_controller.dart';
 
 class FormKulinerScreen extends StatefulWidget {
   const FormKulinerScreen({super.key});
@@ -8,12 +9,14 @@ class FormKulinerScreen extends StatefulWidget {
 }
 
 class _FormKulinerScreenState extends State<FormKulinerScreen> {
+    final KulinerController _controller = KulinerController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Daftar Kuliner"),
       ),
+      
     );
   }
 }
