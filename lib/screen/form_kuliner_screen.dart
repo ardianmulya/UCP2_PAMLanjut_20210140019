064a5be5015d7fe863b4657b341655eab629db51
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 19:47:35 +0700

    membuat class FormKulinerScreen

---
+++ lib/screen/form_kuliner_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FormKulinerScreen extends StatefulWidget {
+  const FormKulinerScreen({super.key});
+
+  @override
+  State<FormKulinerScreen> createState() => _FormKulinerScreenState();
+}
+
+class _FormKulinerScreenState extends State<FormKulinerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
