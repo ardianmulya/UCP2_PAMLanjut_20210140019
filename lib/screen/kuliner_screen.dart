279a153f8d0aca44ccc99177c3d427c79c9b56fd
ardianmulya <ardian.mulya7@gmail.com>
2024-05-10 00:37:50 +0700

    membuat button edit

--- lib/screen/kuliner_screen.dart
+++ lib/screen/kuliner_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ucp2_pam/controller/kuliner_controller.dart';
 import 'package:ucp2_pam/model/kuliner.dart';
+import 'package:ucp2_pam/screen/edit_kuliner_screen.dart';
 import 'package:ucp2_pam/screen/form_kuliner_screen.dart';
 import 'package:ucp2_pam/screen/kuliner_detail_screen.dart';
 
@@ -42,20 +44,47 @@ class _KulinerScreenState extends State<KulinerScreen> {
                 itemCount: snapshot.data?.length ?? 0,
                 itemBuilder: (context, index) {
                   Kuliner kuliner = snapshot.data![index];
-                  return ListTile(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              KulinerDetailScreen(kuliner: kuliner),
-                        ),
-                      );
-                    },
-                    title: Text(kuliner.nama),
-                    subtitle: Text(kuliner.lokasi),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(kuliner.foto),
+                  return Container(
+                    height: 150,
+                    child: Card(
+                      elevation: 5,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      KulinerDetailScreen(kuliner: kuliner),
+                                ),
+                              );
+                            },
+                            title: Text(kuliner.nama),
+                            subtitle: Text(kuliner.lokasi),
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(kuliner.foto),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 85)),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      EditKulinerScreen(kuliner: kuliner),
+                                ),
+                              );
+                                },
+                                child: Icon(Icons.edit),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   );
                 },
