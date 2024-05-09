89435cba22f47bccfe872b5caf26b48b07a77876
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 19:54:26 +0700

    merubah nama class

--- lib/screen/form_kuliner_screen.dart
+++ lib/screen/form_kuliner_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ucp2_pam/controller/kuliner_controller.dart';
-import 'package:ucp2_pam/model/kuliner.dart';
 
 class FormKulinerScreen extends StatefulWidget {
   const FormKulinerScreen({super.key});
@@ -10,56 +8,8 @@ class FormKulinerScreen extends StatefulWidget {
 }
 
 class _FormKulinerScreenState extends State<FormKulinerScreen> {
-  final KulinerController _controller = KulinerController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.getKuliner();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Daftar Kuliner"),
-      ),
-       body: FutureBuilder<List<Kuliner>>(
-          future: _controller.getKuliner(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  Kuliner kuliner = snapshot.data![index];
-                  return ListTile(
-                    onTap: (){},
-                    title: Text(kuliner.nama),
-                    subtitle: Text(kuliner.lokasi),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(kuliner.foto),
-                    ),
-                  );
-                },
-              );
-            }
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const FormKulinerScreen()));
-          },
-          child: const Icon(Icons.add),
-        )
-    );
+    return const Placeholder();
   }
 }
