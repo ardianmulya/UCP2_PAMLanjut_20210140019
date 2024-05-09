5dc661f8616733bee244fc191a5e4d5ca316ca2c
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 19:48:21 +0700

    membuat class KulinerScreen

---
+++ lib/screen/kuliner_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KulinerScreen extends StatefulWidget {
+  const KulinerScreen({super.key});
+
+  @override
+  State<KulinerScreen> createState() => _KulinerScreenState();
+}
+
+class _KulinerScreenState extends State<KulinerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
