5dc46df246c1f2c53473da453f33a32565e6449b
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 18:52:26 +0700

    membuat function getUri

--- lib/service/kuliner_service.dart
+++ lib/service/kuliner_service.dart
@@ -2,4 +2,8 @@ class KulinerService {
 
   final String baseUrl = 'http://192.168.224.122/api/';
   final String endpoint = 'kuliner';
+
+    Uri getUri(String path) {
+    return Uri.parse("$baseUrl$path");
+  }
 }
