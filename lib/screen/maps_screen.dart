d5341ae1f9f377bd4262b362c34ec7611dd33ff5
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 20:09:43 +0700

    membuat variabel mapcontroller dan lastmapposition

--- lib/screen/maps_screen.dart
+++ lib/screen/maps_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsScreen extends StatefulWidget {
     final Function(String) onLocationSelected;
@@ -9,8 +10,15 @@ class MapsScreen extends StatefulWidget {
 }
 
 class _MapsScreenState extends State<MapsScreen> {
+  late GoogleMapController mapController;
+  LatLng? lastMapPosition;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pilih Lokasi"),
+      ),
+    );
   }
 }
