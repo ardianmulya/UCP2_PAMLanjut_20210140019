4cc9978fe01761b69f19b936e7b1b7561842c857
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 20:13:28 +0700

    membuat function initState dan getCurrentLocation

--- lib/screen/maps_screen.dart
+++ lib/screen/maps_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsScreen extends StatefulWidget {
@@ -12,6 +13,27 @@ class MapsScreen extends StatefulWidget {
 class _MapsScreenState extends State<MapsScreen> {
   late GoogleMapController mapController;
   LatLng? lastMapPosition;
+  
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+    Future<void> getCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+    });
+
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      lastMapPosition = LatLng(position.latitude, position.longitude);
+    });
+    mapController.animateCamera(CameraUpdate.newLatLng(lastMapPosition!));
+  }
 
   @override
   Widget build(BuildContext context) {
