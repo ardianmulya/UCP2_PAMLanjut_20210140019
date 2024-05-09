55afb17d2b0201190e94d916e5c10fab2bf1ee28
ardianmulya <ardian.mulya7@gmail.com>
2024-05-09 18:56:00 +0700

    membuat function updatePerson

--- lib/service/kuliner_service.dart
+++ lib/service/kuliner_service.dart
@@ -53,4 +53,19 @@ class KulinerService {
       },
     );
   }
+    Future<http.Response> updatePerson(
+      String id, Map<String, String> data, File? file) async {
+    var request = http.MultipartRequest(
+      'PUT',
+      getUri('$endpoint/$id'),
+    )
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
+
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
+    }
+
+    return await http.Response.fromStream(await request.send());
+  }
 }
